64529a78f483066511876a04b689875897fdfe4c
Rasoul <r.beheshti.f@gmail.com>
2026-07-15 14:17:55 +0500

    fix: package shared config in production images

---
+++ packages/config/src/index.d.ts
@@ -0,0 +1,9 @@
+export declare const officialSocialLinks: Readonly<{
+  instagram: "https://instagram.com/therashpod";
+  telegram: "https://t.me/therashpod";
+}>;
+
+export declare const officialSocialAccessibleNames: Readonly<{
+  instagram: "RashPOD on Instagram";
+  telegram: "RashPOD on Telegram";
+}>;
---
+++ packages/config/src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const officialSocialLinks = Object.freeze({
+  instagram: "https://instagram.com/therashpod",
+  telegram: "https://t.me/therashpod",
+});
+
+const officialSocialAccessibleNames = Object.freeze({
+  instagram: "RashPOD on Instagram",
+  telegram: "RashPOD on Telegram",
+});
+
+module.exports = { officialSocialLinks, officialSocialAccessibleNames };
